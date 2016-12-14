61b9608c4817d2660d8e9ed8337a48c29b7aab5d
cmd-kvn <wong.kevin@student.clackamas.edu>
2016-12-13 17:58:04 -0800

    finished prototype toHtml and appended objects to html

--- scripts/article.js
+++ scripts/article.js
@@ -14,3 +14,33 @@ function Blog (arr) {
   this.link = arr.link;
   this.body = arr.body;
 }
+
+Blog.prototype.toHtml = function() {
+  // find the beginning of the DOM node and all its children where you will fill in the html
+  var $newBlog = $('article.blog-template').clone();
+  // change the attribute 'data-tag' to match the passed-in object
+  $newBlog.attr('data-tag', this.tag);
+  // find() the elements in the clone and fill in the rest of the current template clone with properties from this particular Blog instance
+  $newBlog.find('.title').html(this.title);
+  $newBlog.find('a').html(this.date);
+  $newBlog.find('a').attr('href', this.link);
+  $newBlog.find('.blog-body').html(this.body);
+
+  // remove the class from the clone which is saying in css to not display
+  $newBlog.removeAttr('class');
+
+  return $newBlog;
+};
+
+// sorts blog entries by date but the date value needs yyyy-mm-dd notation
+// blogEntries.sort(function(curElem, nextElem) {
+//   return (new Date(nextElem.publishedOn)) - (new Date(curElem.publishedOn));
+// });
+
+blogEntries.forEach(function(ele) {
+  blogsArr.push(new Blog(ele));
+});
+
+blogsArr.forEach(function(a) {
+  $('#blog-content').append(a.toHtml());
+});
--- scripts/mainArticles.js
+++ scripts/mainArticles.js
@@ -9,17 +9,17 @@ var blogEntries = [
     body: '<p>First blog entry Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
   },
   {
-    Title: 'Second blog entry',
-    Date: December 25, 2016,
-    'Filed under': 'holidays',
-    Link: 'https://www.christmas.com',
-    Body: '<p>It\'s Christmas Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    title: 'Second blog entry',
+    date: 'December 25, 2016',
+    tag: 'holidays',
+    link: 'https://www.christmas.com',
+    body: '<p>It\'s Christmas Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
   },
   {
-    Title: 'Third blog entry',
-    Date: December 31, 2016,
-    'Filed under': 'holidays',
-    Link: 'https://www.timeanddate.com/counters/newyear.html',
-    Body: '<p>It\'s NYE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+    title: 'Third blog entry',
+    date: 'December 31, 2016',
+    tag: 'holidays',
+    link: 'https://www.timeanddate.com/counters/newyear.html',
+    body: '<p>It\'s NYE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
   }
 ];
