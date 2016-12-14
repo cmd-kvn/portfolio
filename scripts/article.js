38cde2c3b9b74aac7879ce6909eee3d891380773
cmd-kvn <wong.kevin@student.clackamas.edu>
2016-12-13 16:20:39 -0800

    finished constructor function for blog entries object array

--- scripts/article.js
+++ scripts/article.js
@@ -3,3 +3,14 @@
 // Leave as little in the window (global) namespace as possible: attach functions to objects, etc.
 // Use jQuery to clone the example markup for each project, as you add additional content.
 // Your "Project" prototype should have a .toHtml() function that adds new data to the DOM.
+
+var blogsArr = [];
+
+// constructor that will take in the blogEntries object array
+function Blog (arr) {
+  this.title = arr.title;
+  this.date = arr.date;
+  this.tag = arr.tag;
+  this.link = arr.link;
+  this.body = arr.body;
+}
