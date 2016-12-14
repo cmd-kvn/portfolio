be5ea3425d2f55fe2351b9dd0f97e01b25be586a
cmd-kvn <wong.kevin@student.clackamas.edu>
2016-12-13 16:07:59 -0800

    initialized js files and built array with three objects

---
+++ scripts/article.js
@@ -0,0 +1,5 @@
+// Create a new .js file with the appropriate methods to help modify your source data to help render it to the DOM:
+// Use good Object Oriented code: Create a constructor function for projects or source data.
+// Leave as little in the window (global) namespace as possible: attach functions to objects, etc.
+// Use jQuery to clone the example markup for each project, as you add additional content.
+// Your "Project" prototype should have a .toHtml() function that adds new data to the DOM.
---
+++ scripts/mainArticles.js
@@ -0,0 +1,25 @@
+// Think of some ideas for your source data, and code it up in its own file! Build out an array of three objects to start with, all having similar properties.
+
+var blogEntries = [
+  {
+    Title: 'First blog entry',
+    Date: December 13, 2016,
+    'Filed under': 'stuff',
+    Link: 'http://www.google.com',
+    Body: '<p>First blog entry Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+  {
+    Title: 'Second blog entry',
+    Date: December 25, 2016,
+    'Filed under': 'holidays',
+    Link: 'https://www.christmas.com',
+    Body: '<p>It\'s Christmas Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  },
+  {
+    Title: 'Third blog entry',
+    Date: December 31, 2016,
+    'Filed under': 'holidays',
+    Link: 'https://www.timeanddate.com/counters/newyear.html',
+    Body: '<p>It\'s NYE Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+  }
+];
