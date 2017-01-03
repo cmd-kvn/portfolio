4fc69332683410d40ee4d70cda700c74ea1b9438
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-02 17:35:52 -0800

    worked on functionality of project and welcome controllers

--- scripts/controllers/projectController.js
+++ scripts/controllers/projectController.js
@@ -3,15 +3,15 @@
 
   projectController.reveal = function() {
     // use jquery to grab and hide all the first children of parent main in the main div html, filter to select the project id, and show it
-    //$('main > *').hide().filter('#project-content').show();
+    $('main > *').hide().filter('#project-content').show();
     // ^this doesn't work for some reason??
-    
-    $('div[class="welcome-box"]').hide();
-    $('section[id="about-me-content"]').hide();
-    $('section[id="blog-content"]').hide();
-    $('section[id="connect-content"]').hide();
 
-    $('section[id="project-content"]').show();
+    // $('div[class="welcome-box"]').hide();
+    // $('section[id="about-me-content"]').hide();
+    // $('section[id="blog-content"]').hide();
+    // $('section[id="connect-content"]').hide();
+    //
+    // $('section[id="project-content"]').show();
 
   };
 
--- scripts/controllers/welcomeController.js
+++ scripts/controllers/welcomeController.js
@@ -3,15 +3,15 @@
 
   welcomeController.reveal = function() {
     // use jquery to grab and hide all the first children of parent main in the main div html, filter to select the class welcome-box, and show it
-    //$('main > *').hide().filter('.welcome-box').show();
+    $('main > *').hide().filter('.welcome-box').show();
     // ^this is not working for some reason???
 
-    $('section[id="about-me-content"]').hide();
-    $('section[id="blog-content"]').hide();
-    $('section[id-"project-content"]').hide();
-    $('section[id="connect-content"]').hide();
-
-    $('div[class="welcome-box"]').show();
+    // $('section[id="about-me-content"]').hide();
+    // $('section[id="blog-content"]').hide();
+    // $('section[id="project-content"]').hide();
+    // $('section[id="connect-content"]').hide();
+    //
+    // $('div[class="welcome-box"]').show();
   };
 
   module.welcomeController = welcomeController;
