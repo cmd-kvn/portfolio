8bd42d1cc90d31bf56297d49a6c0d3b426f2cbaa
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-02 16:43:09 -0800

    added more controller files

--- scripts/controllers/aboutController.js
+++ scripts/controllers/aboutController.js
@@ -5,9 +5,9 @@
     // use jquery to grab and hide all the first children of parent main in the main div html
     // other options would be to give each html element a matching role or data attribute with the same value
     // ex. data="tab" or role="tabpanel"
-    $('main > *').hide();
+    $('main > *').hide().filter('#about-me-content').show(); // chaining the jquery eliminates line 10 $(id).show()
     // use jquery to show the about section in main html
-    $('#about-me-content').show();
+    //$('#about-me-content').show();
   };
 
   module.aboutController = aboutController;
