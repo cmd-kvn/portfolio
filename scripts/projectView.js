4f516da31ac4a6703a345c76a389627e395d8b8c
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-04 10:56:11 -0800

    fixed the append process

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -3,15 +3,17 @@
 
   var projectCompiler = function(currentValue){
     var source = $('#project-handlebars').html();
+    console.log('source: ', source);
     var template = Handlebars.compile(source);
     var html = template(currentValue);
+    console.log('html: ', html);
     // $('#project-handlebars').append(html); // append happens in renderRepos
     return html; // so it can be appended in renderRepos
   };
 
   projectView.renderProjects = function() {
     console.log('inside renderprojects ', projectsObj.withTheAttribute('name'));
-    $('#about ul').empty().append(
+    $('#project-ul').empty().append(
       projectsObj.withTheAttribute('language')
       .map(projectCompiler)
     );
