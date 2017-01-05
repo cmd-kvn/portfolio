090d4c67ba2fcfc84da796eb359af8c885328d46
cmd-kvn <wong.kevin@student.clackamas.edu>
2017-01-04 16:59:16 -0800

    created heroku app exported API key to local env set heroku  git remote started to modify ajax call in project.js

--- scripts/project.js
+++ scripts/project.js
@@ -4,20 +4,30 @@
   projectsObj.allProjects = [];
 
   projectsObj.requestProjects = function(callback) {
-    $.ajax('https://api.github.com/user/repos', {
-      method: 'GET', // usually default
-      headers: {Authorization: 'token ' + githubToken}, // this or parameters version
-      //data: {access_token: githubToken}, // parameters version, developer.github.com/v3/
-      success: function(response) {
-        projectsObj.allProjects = response;
-        console.log('ajax success ', response);
-        console.log('projectsObj.allProjects: ', projectsObj.allProjects);
-        callback();
-      },
-      error: function(response) {
-        console.log('ajax error', response);
-      }
-    });
+    // $.ajax('https://api.github.com/user/repos', {
+    //   method: 'GET', // usually default
+    //   headers: {Authorization: 'token ' + githubToken}, // this or parameters version
+    //   //data: {access_token: githubToken}, // parameters version, developer.github.com/v3/
+    //   success: function(response) {
+    //     projectsObj.allProjects = response;
+    //     console.log('ajax success ', response);
+    //     console.log('projectsObj.allProjects: ', projectsObj.allProjects);
+    //     callback();
+    //   },
+    //   error: function(response) {
+    //     console.log('ajax error', response);
+    //   }
+    // });
+    $.get('/github/user/repos' + //users/repos is the authenticated version
+          '?per_page=10&sort=updated')
+          .done(function(data) {
+            reposObj.allRepos = data;
+            // $.get('/github/user/followers') // second ajax call
+            // .done(function(data) {
+            //   reposObj.followers = data;
+          })
+          .done(callback); // the callback will be the renderRepos
+          // });
   };
 
   projectsObj.withTheAttribute = function(myAttr) {
--- server.js
+++ server.js
@@ -1,15 +1,50 @@
-// Let's build a server!
 var express = require('express'),
+  // DONE: require in our request proxy module
+  requestProxy = require('express-request-proxy'), // library requre('') is assigned to requestProxy
   port = process.env.PORT || 3000,
   app = express();
+
+// DONE: now use our proxy within a function to request
+//        our github data on the server.
+
+function proxyGitHub(request, response) {
+  console.log('Routing Github request for', request.params[0]);
+  // console.log('api key is: ', API_KEY);
+  // console.log('process.env: ', process.env);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    query: {
+      access_token: process.env.API_KEY // the API_KEY:value is a property on the process.env obj
+    }
+  }))(request, response);
+};
+
+app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
-  response.sendFile('index.html', {root: '.'});
+  response.sendFile('index.html', { root: '.' });
 });
 
 app.listen(port, function() {
   console.log('Server started on port ' + port + '!');
 });
+
+
+// // Let's build a server!
+// var express = require('express'),
+//   port = process.env.PORT || 3000,
+//   app = express();
+//
+// app.use(express.static('./'));
+//
+// app.get('*', function(request, response) {
+//   console.log('New request:', request.url);
+//   response.sendFile('index.html', {root: '.'});
+// });
+//
+// app.listen(port, function() {
+//   console.log('Server started on port ' + port + '!');
+// });
